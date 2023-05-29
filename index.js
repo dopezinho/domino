42f187514ffd70c6234d029298453c6612a8d483
HenriqueFStephan <henrique.f.stephan@gmail.com>
2023-05-29 13:56:22 -0300

    1

--- index.js
+++ index.js
@@ -235,3 +235,22 @@ const buttons = document.querySelector("#hamb-button");
     totalClick2(0)
     totalClick3(0)
   })
+
+
+
+const x = document.querySelectorAll('.a')
+const v = document.querySelectorAll('.v')
+
+
+function clicking() {
+  for (i = 1; i <= sumValue1/5; i++) {
+    x[i-1].style.stroke = '#fff'
+    if (sumValue1 % 50 === 0){
+      v[i/10-1].style.stroke = '#fff'
+    }
+    if (sumValue1 === 200) {
+      x1.style.stroke = "#00ff2a"
+    }
+  }
+}
+
